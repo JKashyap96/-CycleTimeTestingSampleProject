0b5b8e9bc470151d4c5ae43091cf3b3af0583017
Jayanta Kashyap <88306913+JKashyap96@users.noreply.github.com>
2021-11-16 16:12:43 +0530

    added 3rd set of test cases

--- test.py
+++ test.py
@@ -15,3 +15,8 @@ def TestAdd():
         assert Sub(6,3) == 3
         assert Mul(2,45) == 90
         assert Div(6,3) == 2
+        #3rd Set
+        assert Add(5,3) == 8
+        assert Sub(90,3) == 87
+        assert Mul(2,.5) == 1
+        assert Div(6,4) == 1.5
